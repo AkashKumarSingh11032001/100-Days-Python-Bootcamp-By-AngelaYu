03f3e287c7caaad2f734813a6c4c611268d022e8
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-02 08:57:32 +0530

    Day1 Done

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/FinalDayProjects.py
@@ -0,0 +1,6 @@
+# final Day Project !!!
+print("What's name of the city you grew up in?")
+name = input()
+print("What's your pet's name?")
+pet = input()
+print("Your band name could be "+ name +" "+ pet)
---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/basic.py
@@ -0,0 +1,116 @@
+# video 4
+
+print("Hello World!")
+
+print("***")
+
+# video 5
+# Ex 1
+
+'''
+## Printing to the Console
+
+# Instructions
+
+Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function. 
+
+**Warning:** The output in your program should match the example output shown below exactly, character for character, even spaces and symbols should be identical, otherwise the tests won't pass.
+
+# Example Output
+
+After you have written your code, you should run your program and it should print the following:
+
+```
+Day 1 - Python Print Function
+```
+
+```
+The function is declared like this:
+```
+
+```
+print('what to print')
+```
+
+e.g. When you hit **run**, this is what should happen:
+![](https://cdn.fs.teachablecdn.com/q89uzhvRTf6CZHLtxLm6)
+ 
+# Test Your Code
+
+Before checking the solution, try copy-pasting your code into this repl: 
+
+[https://repl.it/@appbrewery/day-1-1-test-your-code](https://repl.it/@appbrewery/day-1-1-test-your-code)
+
+This repl includes my testing code that will check if your code meets this assignment's objectives. 
+
+# Solution
+
+[https://repl.it/@appbrewery/day-1-1-solution](https://repl.it/@appbrewery/day-1-1-solution)
+'''
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+print("***")
+
+# video 6
+
+#new line
+print("Hello\nWorld")
+
+# concatination
+print("Hello " + "World " + "!")
+print("***")
+
+# video 7
+# Ex-2
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+"sign.') 
+print('e.g. print("Hello" + "world")')
+print("New lines can be created with a backslash and n.")
+print("***")
+# video 8 
+#input from user
+
+#print(input("Enter your name: "))
+
+# video 9
+# Ex-3
+
+#print(len(input()))
+print("***")
+
+
+# video 10
+name = input()
+print(name)
+name = "Aksingh"
+print(name)
+print("***")
+
+# video 10
+# Ex-4
+a= eval(input())
+b = eval(input())
+print("a: ", a)
+print("b: ", b)
+c= a
+a=b
+b =c
+print("a: ", a)
+print("b: ", b)
+
+#video 11
+# all about nameing convention
+
+#video 12
+# quize
+
+# final Day Project !!!
+print("What's name of the city you grew up in?")
+name = input()
+print("What's your pet's name?")
+pet = input()
+print("Your band name could be "+ name + pet)
