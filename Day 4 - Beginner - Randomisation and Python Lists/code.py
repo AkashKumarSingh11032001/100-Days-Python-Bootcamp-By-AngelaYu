b8d27aad260a79042f617c59ba6af5b949648722
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-04 12:51:41 +0530

    Day4

--- Day 4 - Beginner - Randomisation and Python Lists/code.py
+++ Day 4 - Beginner - Randomisation and Python Lists/code.py
@@ -34,3 +34,26 @@ import random
 # # or
 # rand = random.choice(ind_name)
 # print("bill will be payed by: "+ rand)
+
+# nested list
+
+# ex-4.3
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+# row1 = ["1","2","3"]
+# row2 = ["4","5","6"]
+# row3 = ["7","8","9"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+
+row = int(position[0]) 
+col = int(position[1])
+
+map[row][col] = 'X'
+
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
