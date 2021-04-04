f2e6ab1fa540a389757d3c8710e66b1143c8a4fc
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-04 12:06:54 +0530

    Day4

--- Day 4 - Beginner - Randomisation and Python Lists/code.py
+++ Day 4 - Beginner - Randomisation and Python Lists/code.py
@@ -2,5 +2,35 @@
 # Random modulee
 import random
 
-num = random.randint(1,10)
-print(num)
+# // int num
+# num = random.randint(1,10)
+# print(num)
+
+# float num
+# num_f = random.random()
+# print(num_f)
+
+# ex-4.1
+# num = random.randint(0,1)
+# if(num == 1):
+#     print("Heads")
+# else:
+#     print("Tails")
+
+
+# LIST
+
+# name = ['A','B','C','D','E']
+# print(name[0])
+# name.append('F')
+# name.extend(['a','b','c'])
+# print(name)
+
+# ex-4.2
+
+# name = input("Enter your friend name separted by comma: ")
+# ind_name = name.split(",")
+# # rand = random.randint(0,len(ind_name)-1)
+# # or
+# rand = random.choice(ind_name)
+# print("bill will be payed by: "+ rand)
