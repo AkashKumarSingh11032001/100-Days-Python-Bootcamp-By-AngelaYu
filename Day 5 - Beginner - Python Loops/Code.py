537b2d0e5c1409675d8eae03bbee31cfd12f7ef5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-05 16:36:00 +0530

    Day5

--- Day 5 - Beginner - Python Loops/Code.py
+++ Day 5 - Beginner - Python Loops/Code.py
@@ -1,4 +1,16 @@
-fruits = ['a','b','c']
+#1
+# for loop
+# fruits = ['a','b','c']
 
-for i in fruits:
-    print(i)
+# for i in fruits:
+#     print(i)
+#     print(i + " pie")
+
+#2 ex-5.1
+student_heights = [180, 124, 165, 173, 189, 169, 146]
+sum =0
+for height in student_heights:
+    sum += height
+avg = sum/len(student_heights)
+
+print(round(avg))
