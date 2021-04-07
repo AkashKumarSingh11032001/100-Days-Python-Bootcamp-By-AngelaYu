d852bf32c043f857996f905377cfd19f8af372c5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 09:23:52 +0530

    Day7

--- Day 7 - Beginner - Hangman/Code.py
+++ Day 7 - Beginner - Hangman/Code.py
@@ -8,3 +8,9 @@ word_list = ["abcd","efgh","ijkl"]
 rand_word = random.choice(word_list)
 
 print(rand_word)
+
+user_word = input("Guess any word: ")
+if(user_word in rand_word):
+    print("YES")
+else:
+    print("NO")
