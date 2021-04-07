89433bc7a9474bfa934504077355abd9baffa2fb
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-07 11:35:59 +0530

    Day7

--- Day 7 - Beginner - Hangman/Code.py
+++ Day 7 - Beginner - Hangman/Code.py
@@ -4,13 +4,26 @@ import random
 import string
 
 
-word_list = ["hello","how","are","you"]
-rand_word = random.choice(word_list)
+word_list = ["hello", "how", "are", "you"]
+chosen_word = random.choice(word_list)
+print(chosen_word)
+length = len(chosen_word)
 
-user_word = input("Guess any word: ").lower()
+display = []
+for i in range(length):
+    display.append("_")
+print(display)
 
-for letter in rand_word:
-    if letter == user_word:
-        print("Right")
-    else:
-        print("Wrong")
+while length > 0:
+    user_word = input("Guess any word: ").lower()
+
+    for pos in range(len(chosen_word)):
+        letter = chosen_word[pos]
+        if letter == user_word:
+            display[pos] = letter
+
+    print(display)
+
+    length -= 1
+
+print("you Win!!!")
