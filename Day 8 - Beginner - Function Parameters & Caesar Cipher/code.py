d3811347b6e24c08f31b94b2759c89ab8bcf149f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-08 10:40:10 +0530

    Day8

--- Day 8 - Beginner - Function Parameters & Caesar Cipher/code.py
+++ Day 8 - Beginner - Function Parameters & Caesar Cipher/code.py
@@ -3,3 +3,10 @@ def greet(str):
     print(f"Hello {str}\n"+ f"Bye {str}\n"+ f"See you {str}")
 
 greet("Akash")
+
+# multiple input
+
+def mul_input(str1,str2):
+    print(f"{str1} loves {str2}")
+
+mul_input("Akash","Archana")
