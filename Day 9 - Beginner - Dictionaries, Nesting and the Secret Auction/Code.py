32eb9f80e68b1d8042285b44c4614809de104815
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-09 10:33:59 +0530

    Day9

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/Code.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/Code.py
@@ -1,1 +1,17 @@
 #1 Day9
+dict = {1:'A',2:'B',3:'C',4:'D',5:'E'}
+print(dict[1])
+print(dict[2])
+#  UPDATING DATA
+# Before
+print(dict[3])
+dict[3] = 'F'
+# After
+print(dict[3])
+
+# Adding data
+dict[6] = 'G'
+print(dict)
+
+# # empty the disctnory
+# dict = {}
