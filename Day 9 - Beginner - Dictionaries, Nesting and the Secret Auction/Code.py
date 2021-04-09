2d6d4c525f509ef2c67ee0017132086d57a3836a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-09 10:45:28 +0530

    Day9

--- Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/Code.py
+++ Day 9 - Beginner - Dictionaries, Nesting and the Secret Auction/Code.py
@@ -1,23 +1,53 @@
-#1 Day9
-dict = {1:'A',2:'B',3:'C',4:'D',5:'E'}
-print(dict[1])
-print(dict[2])
-#  UPDATING DATA
-# Before
-print(dict[3])
-dict[3] = 'F'
-# After
-print(dict[3])
+# #1 Day9
+# dict = {1:'A',2:'B',3:'C',4:'D',5:'E'}
+# print(dict[1])
+# print(dict[2])
+# #  UPDATING DATA
+# # Before
+# print(dict[3])
+# dict[3] = 'F'
+# # After
+# print(dict[3])
 
-# Adding data
-dict[6] = 'G'
-print(dict)
+# # Adding data
+# dict[6] = 'G'
+# print(dict)
 
-# # empty the disctnory
-# dict = {}
+# # # empty the disctnory
+# # dict = {}
 
-#  LOOPING THROUGH LOOOP
+# #  LOOPING THROUGH LOOOP
 
-for i in dict:
-    print(i) # Will print "key" itself
-    print(dict[i]) # will print "vallue"
+# for i in dict:
+#     print(i) # Will print "key" itself
+#     print(dict[i]) # will print "vallue"
+
+
+# ex-9.1
+student_scores = {
+
+    "Harry": 81,
+
+    "Ron": 78,
+
+    "Hermione": 99,
+
+    "Draco": 74,
+
+    "Neville": 62,
+
+}
+
+grade = {}
+
+for i in student_scores:
+    if(student_scores[i] >= 91):
+        grade[i] = "OUTSTANDING"
+    elif(student_scores[i] >= 81 and student_scores[i] <= 90):
+        grade[i] = "EXCEED EXPECTATION"
+    elif(student_scores[i] >= 71 and student_scores[i] <=80):
+        grade[i] = "EXCEED EXPECTATION"
+    elif(student_scores[i] <= 70):
+        grade[i] = "EXCEED EXPECTATION"
+
+
