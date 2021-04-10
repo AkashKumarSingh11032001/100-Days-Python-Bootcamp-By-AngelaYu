25e7f5cd5e07689c1328a7b4b6c394fad7c137fe
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-10 19:49:40 +0530

    Day10

--- Day 10 - Beginner - Functions with Outputs/Code.py
+++ Day 10 - Beginner - Functions with Outputs/Code.py
@@ -1,9 +1,9 @@
 #1 
 def formate_name():
-    f_name = input("Enter 1st name: ")
-    l_name = input("Enter  2nd name:")
+    f_name = input("Enter 1st name: ").title()
+    l_name = input("Enter  2nd name:").title()
 
-    return f_name,l_name
+    print(f"Hello {f_name} {l_name}")
 
 
-print(formate_name)
+formate_name()
