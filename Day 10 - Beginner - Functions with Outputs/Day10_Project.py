40eca5593bcde9a5fe202c9ca124366397c6b9ef
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-10 20:29:11 +0530

    Day10

---
+++ Day 10 - Beginner - Functions with Outputs/Day10_Project.py
@@ -0,0 +1,14 @@
+from art import logo
+print(logo)
+
+
+def calc(first,opr,second):
+
+
+
+    
+fir_num = eval(input("Enter the first number: "))
+print("+\n-\n*\n/")
+opr = input("Select Operator: ")
+sec_num = eval(input("Enter the Second number: "))
+
