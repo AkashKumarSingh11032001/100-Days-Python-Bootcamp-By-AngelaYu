141e88ad4078fe519ce4892f869cf04973800495
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-10 20:45:10 +0530

    Day10

--- Day 10 - Beginner - Functions with Outputs/Day10_Project.py
+++ Day 10 - Beginner - Functions with Outputs/Day10_Project.py
@@ -3,12 +3,26 @@ print(logo)
 
 
 def calc(first,opr,second):
+    if(opr == '+'):
+        return first + second
+    elif(opr == '-'):
+        return first - second
+    elif(opr == '*'):
+        return first * second
+    elif(opr == '/'):
+        return first / second
 
 
+test = 'y'
+while(test):
+    fir_num = eval(input("Enter the first number: "))
+    print("+\n-\n*\n/")
+    opr = input("Select Operator: ")
+    sec_num = eval(input("Enter the Second number: "))
 
-    
-fir_num = eval(input("Enter the first number: "))
-print("+\n-\n*\n/")
-opr = input("Select Operator: ")
-sec_num = eval(input("Enter the Second number: "))
+    print(f"Required Output: {fir_num} {opr} {sec_num} = " + str(calc(fir_num,opr,sec_num)))
+
+    rep = input("Want to repeat? (y or n)")
+    if rep == 'y': test = 'y'
+    else: test = 'n'
 
