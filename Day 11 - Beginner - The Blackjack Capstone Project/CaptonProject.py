f1b05ac88b3349ed036ef909c505293870ae2e7c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-12 10:09:11 +0530

    Day11

--- Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
@@ -1,3 +1,17 @@
 from art import logo
+import random
+
+
+def play():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    user_card = random.choices(cards,k=2)
+    print(user_card)
+    comp_card = []
+
 
 want = input("Do you want to PLAY 'y' or 'n': ")
+
+if (want == 'y'):
+    print(logo)
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    play()
