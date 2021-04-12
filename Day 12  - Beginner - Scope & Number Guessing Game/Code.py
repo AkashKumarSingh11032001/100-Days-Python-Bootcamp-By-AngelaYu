6d1391f01d89f8b547aeabb225a62a382ea383f2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-12 13:11:09 +0530

    Day12

--- Day 12  - Beginner - Scope & Number Guessing Game/Code.py
+++ Day 12  - Beginner - Scope & Number Guessing Game/Code.py
@@ -1,9 +1,30 @@
 
-#1
-enemy = 1
-def inc():
-    enemy = 2
-    print(f"inside function {enemy} ")
+# 1
+# enemy = 1
 
-inc()
-print(f"outside fun {enemy} ")
+
+# def inc():
+#     enemy = 2
+#     print(f"inside function {enemy} ")
+
+
+# inc()
+# print(f"outside fun {enemy} ")
+
+# # 2
+# game_level = 3
+# def cre_emy():
+#     enemy = ['a', 'b', 'c']
+#     if game_level < 5:
+#         new_emy = enemy[0]
+
+
+# modify global scope
+a = 1
+def update():
+    global a
+    a += 1
+    print(f"inside {a}")
+
+update()
+print(f"outside {a}")
