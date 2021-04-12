80e20736008bfdd31f7120042e243d52110e1d40
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-12 10:14:44 +0530

    Day11

--- Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
@@ -4,9 +4,14 @@ import random
 
 def play():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    
     user_card = random.choices(cards,k=2)
-    print(user_card)
-    comp_card = []
+    total = sum(user_card)
+    print("Your cards: "+ str(user_card)+", current score: "+str(total))
+    
+    comp_card = random.choices(cards,k=2)
+    print("Computer's first card: "+ str(comp_card[0]))
+
 
 
 want = input("Do you want to PLAY 'y' or 'n': ")
