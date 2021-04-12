5b1732300086cc7b9f396da57232401f2343888e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-12 10:42:36 +0530

    Day11

--- Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
+++ Day 11 - Beginner - The Blackjack Capstone Project/CaptonProject.py
@@ -1,5 +1,16 @@
 from art import logo
 import random
+
+def final_res(user_card,comp_card,card):
+    if(sum(user_card) >= 21):
+        print("    Your Final hand: "+ str(user_card)+ ", final scores: "+ str(sum(user_card)))
+        print("    Computer Final hands: "+ str(comp_card)+", final scores: "+ str(sum(comp_card)))
+        
+    if(sum(user_card) < 21 and sum(user_card) > sum(comp_card)):
+        print("You Win !!!")
+    elif(sum(user_card) >= 21):
+        print("You Loss !!!")
+
 def another(user_card,comp_card,card):
     another_card = input("Type 'y' to get another card, type 'n' to pass: ")
 
@@ -13,12 +24,17 @@ def another(user_card,comp_card,card):
 def game_ext(user_card,comp_card,card):
     
     user_card.append(random.choice(card))
-    print("Your cards: "+ str(user_card)+", current score: "+str(sum(user_card)))
-    print("Computer's first card: "+ str(comp_card[0]))
+    print("    Your cards: "+ str(user_card)+", current score: "+str(sum(user_card)))
+    print("    Computer's first card: "+ str(comp_card[0]))
+    if(sum(user_card) < 21):
+        another(user_card,comp_card,card)
+    else:
+        final_res(user_card,comp_card,card)
+
+
     
-    if(sum(user_card) > 21):
-        print("Your Final hand: "+ str(user_card)+ ", final scores: ")
-    another(user_card,comp_card,card)
+
+    
 
 
 
