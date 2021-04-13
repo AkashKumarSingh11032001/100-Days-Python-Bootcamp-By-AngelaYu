ada10fcdc826bff961761fe12cba84f1b9444fbc
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 14:18:08 +0530

    Day13

--- Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
@@ -54,16 +54,16 @@
 #     print("This is an odd number.")
 
 # ex-13.2
-year = eval(input("Which year do you want to check?"))
+# year = eval(input("Which year do you want to check?"))
 
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0: 
-            print("Leap year .")
-        else:
-            print("Not leap year.")
-    else:
-        print("Leap year.")
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0: 
+#             print("Leap year .")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
 
-else: 
-    print("Not leap year. ")
+# else: 
+#     print("Not leap year. ")
