293be5efa4c32143939cea143b209b0218db7d14
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 14:19:05 +0530

    Day13

--- Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
@@ -67,3 +67,26 @@
 
 # else: 
 #     print("Not leap year. ")
+
+# ex-13.3
+year = input ("Which year do you want to check?")
+
+if year % 4 == 0:
+
+if year 100 == 0:
+
+if year 400 == 0:
+
+print("Leap year.")
+
+else:
+
+print("Not leap year.")
+
+else:
+
+print ("Leap year.")
+
+else:
+
+print ("Not leap year.")
