9e2cf67fbf52b15f9429c89a142e33667181aef1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-13 14:14:10 +0530

    DAy13

--- Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
+++ Day 13 - Beginner - Debugging How to Find and Fix Errors in your Code/Code.py
@@ -46,9 +46,9 @@
 # mutate([1, 2, 3, 5, 8, 13])
 
 # ex-13.1
-number = int(input("Which number do you want to check?"))
+# number = int(input("Which number do you want to check?"))
 
-if number % 2 == 0:
-    print("This is an even number. ") 
-else:
-    print("This is an odd number.")
+# if number % 2 == 0:
+#     print("This is an even number. ") 
+# else:
+#     print("This is an odd number.")
