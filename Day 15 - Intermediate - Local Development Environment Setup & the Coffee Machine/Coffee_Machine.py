5b8b3f8c85f4293fed27700d49dbc9ba840894d5
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-15 12:38:18 +0530

    "cost"

--- Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/Coffee_Machine.py
+++ Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/Coffee_Machine.py
@@ -9,11 +9,11 @@ report = {
     "Milk": 200,
     "Coffee": 100
     },
-    "cost": 2.5
+    "cost": 0
 }
 
 coffee = input("What would you like? (espresso/latte/cappuccino): ")
 
 if(coffee == "report"):
     for key in report["ingredients"]:
-        print(str(key),str(report["ingredients"][key])+ "ml" + report[])
+        print(str(key),str(report["ingredients"][key])+ "ml" + str(report["cost"]) + "$")
