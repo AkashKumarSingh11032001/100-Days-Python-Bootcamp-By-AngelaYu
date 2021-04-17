8e24b8de3958131f09a8d9a4993c13c674ff00b2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-17 12:23:32 +0530

    Day17

--- Day 16  - Intermediate - Object Oriented Programming (OOP)/Day16_Project.py
+++ Day 16  - Intermediate - Object Oriented Programming (OOP)/Day16_Project.py
@@ -4,7 +4,19 @@ from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
 
 coffee_maker.report()
 money_machine.report()
 
+is_on = True
+while is_on:
+    option = menu.get_items()
+    choice = input("What you want? ({option})")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink()
