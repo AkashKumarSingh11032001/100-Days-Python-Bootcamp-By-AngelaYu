c813dec2c4448c09427cac2187808a252d81b07d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-18 09:13:17 +0530

    Day18

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
@@ -1,13 +1,13 @@
 class User:
     #construtor
-    def __init__(self):
-        print("Constructor Called")
+    def __init__(self,user_id):
+        self.id = user_id
 
 
 
 user_1 = User()
-user_1.id = "001"
-user_1.username = "Akash"
+# user_1.id = "001"
+# user_1.username = "Akash"
 
 print(user_1.username)
 
