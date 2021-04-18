cbabd325d673c405cea6ea2d05c4de3b01ea6e4a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-18 12:13:05 +0530

    Day18

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/quize_brain.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/quize_brain.py
@@ -2,6 +2,14 @@ class QuizBrain:
     def __init__(self,q_list):
         self.question_number = 0
         self.question_list = q_list
+    
+    def next_question(self):
+        current_ques = self.question_list[self.question_number]
+        input(f"Q.{self.question_number}: {current_ques.text} (True/False)")
+
+        
+
+
 
 
     
