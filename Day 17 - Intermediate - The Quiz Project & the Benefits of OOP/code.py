14e6982acd9adc820c473870279efc0ec8cb8c32
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-18 10:00:48 +0530

    Day18

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
@@ -4,6 +4,11 @@ class User:
         self.id = user_id
         self.username = user_name
         self.follower = 0
+        self.following = 0
+
+    def follow(self,user):
+        user.follower += 1
+        self.following += 1
 
 
 user_1 = User("001","Akash")
@@ -11,7 +16,14 @@ user_2 = User("002","Angella")
 # user_1.id = "001"
 # user_1.username = "Akash"
 
-print(user_1.username)
-print(user_2.username)
+# print(user_1.username)
+# print(user_2.username)
+# print(user_1.follower)
+
+user_1.follow(user_2)
 print(user_1.follower)
+print(user_1.following)
+print(user_2.follower)
+print(user_2.following)
+
 
