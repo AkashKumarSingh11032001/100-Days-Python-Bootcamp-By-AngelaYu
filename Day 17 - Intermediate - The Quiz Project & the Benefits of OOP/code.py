6523cbbbc6fda3322a26738988af9bc9e5b69781
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-18 09:19:05 +0530

    Day18

--- Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
+++ Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/code.py
@@ -3,7 +3,7 @@ class User:
     def __init__(self,user_id,user_name):
         self.id = user_id
         self.username = user_name
-        self.foolower = 0
+        self.follower = 0
 
 
 user_1 = User("001","Akash")
