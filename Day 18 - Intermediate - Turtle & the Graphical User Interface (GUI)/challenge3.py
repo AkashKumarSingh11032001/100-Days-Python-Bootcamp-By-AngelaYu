1cfd2064473777f00e9f965806a88017ed22b158
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-19 12:52:01 +0530

    Day19

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge3.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge3.py
@@ -4,9 +4,11 @@ tut = Turtle()
 
 for i in range(3,9):
     angle = 360/i
-    for j in range(side):    
+    for j in range(i):    
         tut.forward(100)
         tut.left(angle)
+
+
 
 
 screen = Screen()
