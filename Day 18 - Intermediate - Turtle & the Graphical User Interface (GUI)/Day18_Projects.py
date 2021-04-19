f479c1621da80f43144587ac63e7dea21aff896a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-19 15:31:29 +0530

    Day19

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day18_Projects.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day18_Projects.py
@@ -1,4 +1,8 @@
-from turtle import Screen, Shape, Turtle
+from turtle import Screen, Shape, Turtle, color
 import random
 import turtle
+import colorgram
 
+lis  = []
+lis.append(tuple(colorgram.extract("a.jpg",10)))
+print(lis)
