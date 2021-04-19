41a8801e41f415526120dcd1dddf6ca68f3eebc3
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-19 12:47:10 +0530

    Day19

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge3.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/challenge3.py
@@ -2,10 +2,11 @@ from turtle import Screen, Shape, Turtle
 
 tut = Turtle()
 
-side = 4
+for i in range(9):
+    side = i
+    for j in range(side):
+        angle = 360/side
+        tut.forward(100)
+        tut.left(angle)
 
-for i in range(side):
-    angle = 360/4
-    tut.forward(100)
-    tut.left(angle)
-    
+
