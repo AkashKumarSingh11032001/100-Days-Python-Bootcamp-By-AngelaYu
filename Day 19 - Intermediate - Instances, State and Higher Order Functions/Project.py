81105e6e196daec6da8239541475fa142782748a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-20 17:42:33 +0530

    Day20

--- Day 19 - Intermediate - Instances, State and Higher Order Functions/Project.py
+++ Day 19 - Intermediate - Instances, State and Higher Order Functions/Project.py
@@ -8,10 +8,15 @@ screen = Screen()
 screen.setup(width= 500,height= 400) #scren size
 user_bet = screen.textinput(title="Make your Bet", prompt="Which will win the match") #pop-up
 print(user_bet)
+colors = ["yellow", "gold", "orange", "red", "maroon", "violet", "magenta", "purple" ]
+y_pos = [-100, -80, -60, -40, -20, 0, 20 ,40]
 
-my_tut = Turtle("turtle")
-my_tut.penup()
-my_tut.goto(x=-230,y=-100 )
+
+for _ in range(8):
+    my_tut = Turtle("turtle")
+    my_tut.color(colors[_])
+    my_tut.penup()
+    my_tut.goto(x=-230,y=y_pos[_])
 
 
 screen.exitonclick()
