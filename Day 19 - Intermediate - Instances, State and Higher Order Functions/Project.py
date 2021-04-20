b5dc4d4652500946a90dc962612da9a76dedcd7c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-20 17:31:08 +0530

    Day20

--- Day 19 - Intermediate - Instances, State and Higher Order Functions/Project.py
+++ Day 19 - Intermediate - Instances, State and Higher Order Functions/Project.py
@@ -3,7 +3,7 @@ import random
 import turtle
 import random
 
-my_tut = Turtle()
+
 screen = Screen()
 screen.setup(width= 500,height= 400) #scren size
 user_bet = screen.textinput(title="Make your Bet", prompt="Which will win the match") #pop-up
