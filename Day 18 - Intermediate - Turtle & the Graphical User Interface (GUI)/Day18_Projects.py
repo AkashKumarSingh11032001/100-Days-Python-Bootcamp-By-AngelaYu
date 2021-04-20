170e91de0498c8d6d8c16afebea6667be3e7ff25
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-20 09:48:40 +0530

    Day20

--- Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day18_Projects.py
+++ Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Day18_Projects.py
@@ -1,11 +1,36 @@
 from turtle import Screen, Shape, Turtle, color
 import random
 import turtle
+from PIL.Image import new
 import colorgram
 
-# lis  = []
-# lis.append(tuple(colorgram.extract("a.jpg",10)))
-# print(lis)
-color = colorgram.extract("a.jpg",10)
-print(color)
-ṇ
+# rgb_color = []
+# colors = colorgram.extract("a.jpg",38)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb_color.append(new_color)
+
+# print(rgb_color)
+
+def randomcol():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    
+    random_color = (r,g,b)
+
+    return random_color
+
+
+my_tut = Turtle()
+my_tut.dot(20,randomcol())
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
