9c92aeb02d5b19a5fd6bf040a2a5847541eaa1cc
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-21 13:39:18 +0530

    Day21

--- Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
+++ Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/snake.py
@@ -10,19 +10,19 @@ class Snake:
         self.create_snake()
         #self.move()
 
-        def create_snake(self):
-            for pos in STARTING_POS:
-                snake_body = Turtle("square")
-                snake_body.color("white")
-                snake_body.penup()
-                snake_body.goto(pos)
-                self.snake.append(snake_body)
+    def create_snake(self):
+        for pos in STARTING_POS:
+            snake_body = Turtle("square")
+            snake_body.color("white")
+            snake_body.penup()
+            snake_body.goto(pos)
+            self.snake.append(snake_body)
 
-        def move(self):
-            for seg_num in range(len(self.snake)-1,0,-1):
-                new_x = self.snake[seg_num - 1].xcor()
-                new_y = self.snake[seg_num - 1].ycor()
-                self.snake[seg_num].goto(new_x,new_y)
+    def move(self):
+        for seg_num in range(len(self.snake)-1,0,-1):
+            new_x = self.snake[seg_num - 1].xcor()
+            new_y = self.snake[seg_num - 1].ycor()
+            self.snake[seg_num].goto(new_x,new_y)
 
-            self.snake[0].forward(MOVE_DIS)
+        self.snake[0].forward(MOVE_DIS)
             
