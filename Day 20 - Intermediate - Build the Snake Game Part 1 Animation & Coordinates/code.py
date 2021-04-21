b66590e077723c821009e5354dd280a8a0a940e0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-21 09:53:27 +0530

    Day21

--- Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/code.py
+++ Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/code.py
@@ -5,7 +5,14 @@ screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+starting_pos = [(0,0),(-20,0),(-40,0)]
 # // CREATE SNAKE BODY
+for pos in starting_pos:
+    snake_body = Turtle("square")
+    snake_body.color("white")
+    snake_body.goto(pos)
+
+
 
 
 
