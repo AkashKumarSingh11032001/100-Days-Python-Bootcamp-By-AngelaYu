e913ecd8afef932198def897a08361df83e20017
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-21 13:26:32 +0530

    Day21

--- Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/code.py
+++ Day 20 - Intermediate - Build the Snake Game Part 1 Animation & Coordinates/code.py
@@ -20,13 +20,7 @@ while game_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(snake)-1,0,-1):
-        new_x = snake[seg_num - 1].xcor()
-        new_y = snake[seg_num - 1].ycor()
-        snake[seg_num].goto(new_x,new_y)
-
-    snake[0].forward(20)
-    snake[0].left(90)
+    
 
 
 
