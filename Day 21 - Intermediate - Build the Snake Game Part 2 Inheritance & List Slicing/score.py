913ad8ee0b1b51115e84c439d54f12adc481dcf7
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-23 17:57:19 +0530

    Day23

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/score.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/score.py
@@ -5,4 +5,8 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
         self.write(f"Score: {self.score}",align="center", font=("Arial",24,"normal"))
+        self.hideturtle()
