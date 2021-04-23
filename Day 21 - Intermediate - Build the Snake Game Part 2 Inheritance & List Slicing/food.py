4c03ba87c3fcf9300b32d258ce2ff26da34a0747
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-23 17:45:25 +0530

    Day23

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/food.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/food.py
@@ -10,10 +10,10 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5,stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
-        self.refreh()
+        self.refresh()
 
 
-    def refreh(self):
+    def refresh(self):
         self.goto(random.randint(-280, 280), random.randint(-280, 280))
 
 
