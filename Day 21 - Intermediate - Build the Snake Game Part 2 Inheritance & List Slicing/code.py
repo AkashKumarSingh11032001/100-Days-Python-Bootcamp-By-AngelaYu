a61d4a55adfbd32f61f9495603584ae67fa4bf4c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-23 16:28:45 +0530

    Day23

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/code.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/code.py
@@ -13,10 +13,10 @@ snake = Snake()
 
 # // CREATE SNAKE BODY
 screen.listen()
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.up,"Up")
-screen.onkey(snake.up,"Up")
+screen.onkey(snake.up(),"Up")
+screen.onkey(snake.down(),"Down")
+screen.onkey(snake.left(),"Left")
+screen.onkey(snake.right(),"Right")
 
 
 # // SNAKE MOVEMENT
