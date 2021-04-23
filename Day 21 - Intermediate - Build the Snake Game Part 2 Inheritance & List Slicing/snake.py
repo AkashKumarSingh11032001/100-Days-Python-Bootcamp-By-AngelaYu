2b0a631e33abdc2be7f6fdb117cf936a81f06a8e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-23 15:18:55 +0530

    Day23

---
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/snake.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+STARTING_POS = [(0,0),(-20,0),(-40,0)]
+MOVE_DIS = 20
+
+class Snake:
+
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        #self.move()
+
+    def create_snake(self):
+        for pos in STARTING_POS:
+            snake_body = Turtle("square")
+            snake_body.color("white")
+            snake_body.penup()
+            snake_body.goto(pos)
+            self.snake.append(snake_body)
+
+    def move(self):
+        for seg_num in range(len(self.snake)-1,0,-1):
+            new_x = self.snake[seg_num - 1].xcor()
+            new_y = self.snake[seg_num - 1].ycor()
+            self.snake[seg_num].goto(new_x,new_y)
+
+        self.snake[0].forward(MOVE_DIS)
+            
