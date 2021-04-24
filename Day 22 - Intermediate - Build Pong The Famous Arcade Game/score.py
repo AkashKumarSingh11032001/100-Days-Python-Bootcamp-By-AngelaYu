8aa813bd72cf91beb8539d170edf9673925b2cd9
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 16:15:10 +0530

    Day24

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/score.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/score.py
@@ -1,5 +1,12 @@
 from turtle import Turtle
 
-class Score:
+class Score(Turtle):
 
     def __init__(self):
+        super(Score, self).__init__()
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.l_score = 0
+        self,r_score = 0
+
