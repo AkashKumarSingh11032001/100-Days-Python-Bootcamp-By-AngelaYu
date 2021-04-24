9482fc985556247dbbe8159004d2355446f85736
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 14:46:36 +0530

    Day24

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
@@ -1,9 +1,14 @@
-from turtle import Screen
+from turtle import Screen, Turtle
 
 screen = Screen()
 screen.title("Pong Game")
 screen.setup(width=800,height=600)
 screen.bgcolor("black")
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
 
 
 
