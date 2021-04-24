0a716f42bda9a8d6497f3bcc97aba9114d6493e1
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 13:16:32 +0530

    Day24

--- Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/code.py
+++ Day 21 - Intermediate - Build the Snake Game Part 2 Inheritance & List Slicing/code.py
@@ -37,7 +37,8 @@ while game_on:
 
 
     # // DETECT COLLISION WITH WALL
-    if snake.head.xcor() > 280 or
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+
 
 
 
