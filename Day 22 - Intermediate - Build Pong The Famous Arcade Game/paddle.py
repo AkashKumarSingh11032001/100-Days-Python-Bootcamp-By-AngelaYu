f104e641bcdb62ab2df50010b3957b5927672f83
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 15:23:40 +0530

    Day24

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/paddle.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/paddle.py
@@ -15,7 +15,7 @@ class Paddle(Turtle):
         self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
+        new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
 
 
