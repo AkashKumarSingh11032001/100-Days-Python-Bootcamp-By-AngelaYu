29dc1e850d10483d7122e547f8c20c1a5c20c586
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 16:10:51 +0530

    Day24

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
@@ -35,6 +35,9 @@ while game_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # // Detect when you miss paddle
+    if ball.xcor() > 380:
+        ball.reset_pos()
 
 
 
