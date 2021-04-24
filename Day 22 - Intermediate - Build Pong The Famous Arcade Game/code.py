826d4e69639df041969677b95d09a8e30cf87451
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-24 14:50:20 +0530

    Day24

--- Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
+++ Day 22 - Intermediate - Build Pong The Famous Arcade Game/code.py
@@ -17,8 +17,9 @@ def go_up():
     paddle.goto(paddle.xcor(),new_y)
 
 def go_down():
-    new_y = paddle.ycor()+20
+    new_y = paddle.ycor()-20
     paddle.goto(paddle.xcor(),new_y)
+
 screen.listen()
 screen.onkey(go_up,"Up")
 screen.onkey(go_down,"Down")
