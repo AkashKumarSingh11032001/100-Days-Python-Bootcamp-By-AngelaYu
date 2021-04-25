d7bedeb0566205297e9bcff0cbd758db697d01eb
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-25 21:36:01 +0530

    Day25

--- Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
+++ Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
@@ -1,6 +1,9 @@
 
-with open("Inputs/name.txt") as name:
-    names = name.readline()
+with open("Inputs/name.txt") as inv_name:
+    names = inv_name.readline()
 
 with open("Inputs/Letter.txt") as letter:
     letter_cont = letter.read()
+    for name in names:
+
+
