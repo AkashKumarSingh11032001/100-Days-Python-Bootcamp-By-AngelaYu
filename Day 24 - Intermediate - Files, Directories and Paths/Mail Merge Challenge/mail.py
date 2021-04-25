6e20e365c17677daf06775eb7134cb05141e29d2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-25 21:37:21 +0530

    Day25

--- Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
+++ Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
@@ -5,5 +5,8 @@ with open("Inputs/name.txt") as inv_name:
 with open("Inputs/Letter.txt") as letter:
     letter_cont = letter.read()
     for name in names:
+        strip_name = name.strip()
+        new_letter = letter_cont.replace("[name]",strip_name)
+        with open()
 
 
