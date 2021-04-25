da65fe5777b09b4da710ae5c2843913e8c13cf84
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-25 13:59:22 +0530

    Day25

---
+++ Day 24 - Intermediate - Files, Directories and Paths/snake.py
@@ -0,0 +1,59 @@
+from turtle import Turtle
+
+STARTING_POS = [(0,0),(-20,0),(-40,0)]
+MOVE_DIS = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+
+    def __init__(self):
+        self.snake = []
+        self.create_snake()
+        self.head = self.snake[0]
+        #self.move()
+
+    def create_snake(self):
+        for pos in STARTING_POS:
+            self.add_segment(pos)
+
+
+    def add_segment(self,pos):
+        snake_body = Turtle("square")
+        snake_body.color("white")
+        snake_body.penup()
+        snake_body.goto(pos)
+        self.snake.append(snake_body)
+
+    def extend(self):
+        self.add_segment(self.snake[-1].position())
+
+        # add new segment to snake
+
+
+    def move(self):
+        for seg_num in range(len(self.snake)-1,0,-1):
+            new_x = self.snake[seg_num - 1].xcor()
+            new_y = self.snake[seg_num - 1].ycor()
+            self.snake[seg_num].goto(new_x,new_y)
+
+        self.snake[0].forward(MOVE_DIS)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
