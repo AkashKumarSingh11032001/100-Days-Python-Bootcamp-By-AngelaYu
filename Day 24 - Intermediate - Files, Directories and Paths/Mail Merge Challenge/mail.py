5357142aee5aff9d0b7e234e2e37d56ca7170ba4
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-25 21:39:33 +0530

    Day25

--- Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
+++ Day 24 - Intermediate - Files, Directories and Paths/Mail Merge Challenge/mail.py
@@ -7,6 +7,7 @@ with open("Inputs/Letter.txt") as letter:
     for name in names:
         strip_name = name.strip()
         new_letter = letter_cont.replace("[name]",strip_name)
-        with open()
+        with open(f"Output/letter_for_{strip_name}.txt",mode="w") as updated_letter:
+            updated_letter.write(new_letter)
 
 
