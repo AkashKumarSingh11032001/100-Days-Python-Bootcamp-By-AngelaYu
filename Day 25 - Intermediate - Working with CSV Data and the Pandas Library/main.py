c4c52c7dc45abd1012c7d2811908afd2675e759a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-26 14:01:25 +0530

    Day26

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/main.py
@@ -4,7 +4,10 @@
 #     print(file_cont)"""
 
 import csv
+temperature = []
 with open("weather.csv") as data_file:
     data = csv.reader(data_file)
     for row in data:
-        print(row)
+        if row[1] != ' temp':
+            temperature.append(int(row[1]))
+    print(temperature)
