8d42d409d027e1ecba1953f98c3cccf1bc2c6ccf
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-26 21:10:11 +0530

    Day26

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day25 Projct/game.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day25 Projct/game.py
@@ -7,8 +7,11 @@ img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
 
-def get_mouse_click(x,y):
-    print(x,y)
+# // CORDINATE BY CLICKING ON SCREEN
+# def get_mouse_click(x,y):
+#     print(x,y)
+#
+# turtle.onscreenclick(get_mouse_click)
+# turtle.mainloop()
 
-turtle.onscreenclick(get_mouse_click)
-turtle.mainloop()
+answer_state = screen.textinput(title="State name",prompt="Guess State name?")
