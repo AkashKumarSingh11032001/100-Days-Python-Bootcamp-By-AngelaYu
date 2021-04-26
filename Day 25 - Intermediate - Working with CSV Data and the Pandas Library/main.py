4cfd0b65e5f42c11c18d94ba42f89928863ee4cf
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-26 13:00:13 +0530

    Day26

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/main.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/main.py
@@ -1,4 +1,9 @@
-
+#
+# with open("weather.csv") as data_file:
+#     file_cont = data_file.readlines()
+#     print(file_cont)
+import csv
 with open("weather.csv") as data_file:
-    file_cont = data_file.readlines()
-    print(file_cont)
+    data = csv.reader(data_file):
+    for row in data:
+        print(row)
