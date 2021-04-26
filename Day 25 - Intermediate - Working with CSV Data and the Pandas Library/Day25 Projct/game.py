5281b4f9ecbe194110454a3653fc9770a1fe043a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-26 21:06:30 +0530

    Day26

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day25 Projct/game.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Day25 Projct/game.py
@@ -3,8 +3,14 @@ import turtle
 screen = turtle.Screen()
 screen.title("US States")
 
-img = "blank_state_img.gif"
+img = "blank_states_img.gif"
 screen.addshape(img)
 turtle.shape(img)
+
+def get_mouse_click(x,y):
+    print(x,y)
+
+turtle.onscreenclick(get_mouse_click())
+turtle.mainloop()
 
 screen.exitonclick()
