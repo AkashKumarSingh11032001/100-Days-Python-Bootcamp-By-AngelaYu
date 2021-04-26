8c49b3eb19ea005782d89ee297c7f79a73bb35b7
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-26 16:22:19 +0530

    Day26

--- Day 25 - Intermediate - Working with CSV Data and the Pandas Library/GreatSquirell.py
+++ Day 25 - Intermediate - Working with CSV Data and the Pandas Library/GreatSquirell.py
@@ -2,6 +2,11 @@ import pandas
 
 data = pandas.read_csv("squirel_data.csv")
 gray_squ = data[data["Primary Fur Color"] == "Gray"]
+cinnamon_squ = data[data["Primary Fur Color"] == "Cinnamon"]
+black_squ = data[data["Primary Fur Color"] == "Black"]
 
 
-print(gray_squ)
+
+print(len(gray_squ))
+print(len(cinnamon_squ))
+print(len(black_squ))
