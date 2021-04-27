410461afcf39154f8329ef86d43329b8a311ef9c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-27 22:55:15 +0530

    Day27

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/pandaDataframe.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/pandaDataframe.py
@@ -12,6 +12,12 @@ import pandas
 student_dataframe = pandas.DataFrame(student_dict)
 print(student_dataframe)
 
-#loop throw dataframe
+#// loop throw dataframe
+
 # for (key,value) in student_dataframe.items():
 #     print(value)
+
+for(index,row) in student_dataframe.iterrows():
+    print(index)
+    print(row)
+
