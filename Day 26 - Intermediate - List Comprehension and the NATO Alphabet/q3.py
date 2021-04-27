01d30554fd48b181afa2fdbfe6cf058e96307efd
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-04-27 15:38:53 +0530

    Day27

--- Day 26 - Intermediate - List Comprehension and the NATO Alphabet/q3.py
+++ Day 26 - Intermediate - List Comprehension and the NATO Alphabet/q3.py
@@ -4,4 +4,4 @@ with open("q3_file1.txt") as file1:
 with open("q3_file2.txt") as file2:
     file2_data = file2.readlines()
 
-print([i for i in file1_data for j in file2_data if i == j ])
+print([int(i) for i in file1_data if i in file2_data ])
